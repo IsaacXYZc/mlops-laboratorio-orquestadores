b47deeb7fa7210477fd36dbdc7be556f7c3495d8
isaacxyzc <isaacxyzc@gmail.com>
2025-11-14 15:54:00 -0500

    tracking con mlflow

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 import gradio as gr
-import provider.gemini_provider as traductor
+import mlflow
+import provider.gemini_provider as translator
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv(override=True)
+mlflow_tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
 
 def translate_text(text, target_language):
     if(text.strip() == ""):
         raise gr.Error("Por favor ingrese un texto para traducir.", duration=6)
     
-    output_text = traductor.get_traduction(text, target_language)
-    return output_text
+    start_time = time.perf_counter()
+    output_text = translator.get_traduction(text, target_language)
+    end_time = time.time()
+    latency = end_time - start_time
+
+    mlflow.set_tracking_uri(mlflow_tracking_uri)
+    mlflow.set_experiment("text-translator-experiment")
+    with mlflow.start_run():
+        # Log de los parametros
+        mlflow.log_param("target_language", target_language)
+        mlflow.log_param("model", translator.model_name)
+
+        # Log de las métricas
+        mlflow.log_metric("latency", latency)
+        mlflow.log_metric("input_text_length", len(text))
+        mlflow.log_metric("output_text_length", len(output_text))
+
+        # Log de los artefactos
+        mlflow.log_text(text, "input_text.txt")
+        mlflow.log_text(output_text, "output_text.txt")
+    
+    return output_text  
 
 
 with gr.Blocks() as demo:
